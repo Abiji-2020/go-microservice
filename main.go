5ff580a2c16968cc6744bfa43bca93cceb99b2b2
Abinand P <abinand0911@gmail.com>
2024-03-03 07:39:56 +0000

    created a application folder and moved the app.go and routes.go files into it

--- main.go
+++ main.go
@@ -2,26 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-)
+	"context"
+	"github.com/Abiji-2020/go-microservice/application"
+)	
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("Falied to listen to server", err)
-	}
-}
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-
-	w.Write([]byte("Hello Wordl!"))
+	app:= application.New()
+	err:= app.Start(context.TODO())
 }
