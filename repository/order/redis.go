ba3b6c8206bf37d77cd3291211e6b4ffa4744697
Abinand P <abinand0911@gmail.com>
2024-03-05 17:22:50 +0000

    Added update, delete, and set funcations

---
+++ repository/order/redis.go
@@ -0,0 +1,91 @@
+package order
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/Abiji-2020/go-microservice.git/model"
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisRepo struct {
+	Client *redis.Client
+}
+
+func orderIdKey(id uint64) string {
+	return fmt.Sprintf("order:%d", id)
+}
+func (r *RedisRepo) Insert(ctx context.Context, order model.Order) error {
+	data, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to encode order:%w", err)
+	}
+
+	key := orderIdKey(order.OrderId)
+
+	res := r.Client.SetNX(ctx, key, string(data), 0)
+
+	if err := res.Err(); err != nil {
+		return fmt.Errorf("failed to insert order:%w", err)
+	}
+	return nil
+
+}
+
+var ErrNotExist = errors.New("order does not exist")
+
+func (r *RedisRepo) FindById(ctx context.Context, id uint64) (model.Order, error) {
+	key := orderIdKey(id)
+
+	value, err := r.Client.Get(ctx, key).Result()
+	if errors.Is(err, redis.Nil) {
+		return model.Order{}, ErrNotExist
+	} else if err != nil {
+		return model.Order{}, fmt.Errorf("get order: %w", err)
+	}
+
+	var order model.Order
+	err = json.Unmarshal([]byte(value), &order)
+	if err != nil {
+		return model.Order{}, fmt.Errorf("failed to decode json: %w", err)
+	}
+	return order, nil
+}
+
+func (r *RedisRepo) DeleteById (ctx context.Context, id uint64)error{
+	key:=orderIdKey(id)
+
+	err:= r.Client.Del(ctx,key).Err()
+	if errors.Is(err,redis.Nil){
+		return ErrNotExist
+	}else if err!=nil{
+		return fmt.Errorf("failed to delete order: %w",err)
+	}
+	return nil
+}
+
+func (r *RedisRepo) Update(ctx context.Context, order model.Order) error {
+	data, err := json.Marshal(order)
+	if err != nil {
+		return fmt.Errorf("failed to encode order:%w", err)
+	}
+
+	key := orderIdKey(order.OrderId)
+	err = r.Client.SetXX(ctx, key, string(data), 0).Err() // Remove extra arguments
+
+	res := r.Client.Set(ctx, key, string(data), 0)
+
+	if errors.Is(err, redis.Nil) {
+		return ErrNotExist
+	}else if err != nil {
+		return fmt.Errorf("failed to update order:%w", err)
+	}
+	return nil
+}
+
+type FindAllPage struct{
+	
+}
+
+func (r *RedisRepo) FindAll(ctx context.Context) ([]model.Order, error) {
