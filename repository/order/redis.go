d47b6fab010fbc16a7317daa83278ffae245a904
Abinand P <abinand0911@gmail.com>
2024-03-05 17:49:42 +0000

    finished the data model with functions

--- repository/order/redis.go
+++ repository/order/redis.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+
 	"github.com/Abiji-2020/go-microservice.git/model"
+
 	"github.com/redis/go-redis/v9"
 )
 
@@ -23,11 +25,22 @@ func (r *RedisRepo) Insert(ctx context.Context, order model.Order) error {
 	}
 
 	key := orderIdKey(order.OrderId)
+	txn := r.Client.TxPipeline()
 
-	res := r.Client.SetNX(ctx, key, string(data), 0)
+	res := txn.SetNX(ctx, key, string(data), 0)
 
 	if err := res.Err(); err != nil {
+		txn.Discard()
 		return fmt.Errorf("failed to insert order:%w", err)
+	}
+
+	if err := txn.SAdd(ctx, "orders", key).Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to add to orders set :%w", err)
+	}
+
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("failed to exec: %w", err)
 	}
 	return nil
 
@@ -53,14 +66,24 @@ func (r *RedisRepo) FindById(ctx context.Context, id uint64) (model.Order, error
 	return order, nil
 }
 
-func (r *RedisRepo) DeleteById (ctx context.Context, id uint64)error{
-	key:=orderIdKey(id)
+func (r *RedisRepo) DeleteById(ctx context.Context, id uint64) error {
+	key := orderIdKey(id)
+	txn := r.Client.TxPipeline()
 
-	err:= r.Client.Del(ctx,key).Err()
-	if errors.Is(err,redis.Nil){
+	err := txn.Del(ctx, key).Err()
+	if errors.Is(err, redis.Nil) {
+		txn.Discard()
 		return ErrNotExist
-	}else if err!=nil{
-		return fmt.Errorf("failed to delete order: %w",err)
+	} else if err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to delete order: %w", err)
+	}
+	if err := txn.SRem(ctx, "orders", key).Err(); err != nil {
+		txn.Discard()
+		return fmt.Errorf("failed to remove from orders set: %w", err)
+	}
+	if _, err := txn.Exec(ctx); err != nil {
+		return fmt.Errorf("failed to exec: %w", err)
 	}
 	return nil
 }
@@ -74,18 +97,51 @@ func (r *RedisRepo) Update(ctx context.Context, order model.Order) error {
 	key := orderIdKey(order.OrderId)
 	err = r.Client.SetXX(ctx, key, string(data), 0).Err() // Remove extra arguments
 
-	res := r.Client.Set(ctx, key, string(data), 0)
-
 	if errors.Is(err, redis.Nil) {
 		return ErrNotExist
-	}else if err != nil {
+	} else if err != nil {
 		return fmt.Errorf("failed to update order:%w", err)
 	}
 	return nil
 }
 
-type FindAllPage struct{
-	
+type FindAllPage struct {
+	Size   uint
+	Offset uint
 }
 
-func (r *RedisRepo) FindAll(ctx context.Context) ([]model.Order, error) {
+type FindResult struct {
+	Orders []model.Order
+	Cursor uint64
+}
+
+func (r *RedisRepo) FindAll(ctx context.Context, page FindAllPage) (FindResult, error) {
+	res := r.Client.SScan(ctx, "orders", uint64(page.Offset), "*", int64(page.Size))
+	keys, cursor, err := res.Result()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("failed to get order ids:%w", err)
+	}
+	if len(keys) == 0 {
+		return FindResult{
+			Orders: []model.Order{},
+		}, nil
+	}
+	xs, err := r.Client.MGet(ctx, keys...).Result()
+	if err != nil {
+		return FindResult{}, fmt.Errorf("failed to get orders:%w", err)
+	}
+	orders := make([]model.Order, len(xs))
+	for i, x := range xs {
+		x := x.(string)
+		var order model.Order
+		err := json.Unmarshal([]byte(x), &order)
+		if err != nil {
+			return FindResult{}, fmt.Errorf("failed to decode order:%w", err)
+		}
+		orders[i] = order
+	}
+	return FindResult{
+		Orders: orders,
+		Cursor: cursor,
+	}, nil
+}
